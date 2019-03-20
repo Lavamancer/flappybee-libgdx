5e80a2bae28af29d4db28e07f437a23e8761f720
Juan Albarracin <jalbarracinq@gmail.com>
2019-03-20 13:19:28 +0100

    change to flappybee

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-### Asteroids Libgdx
-
-# Este es un proyecto de prueba para nuestro primer juego en la plataforma libgdx
+### FlappyBee Libgdx
--- desktop/src/com/jalbarracin/desktop/DesktopLauncher.java
+++ desktop/src/com/jalbarracin/desktop/DesktopLauncher.java
@@ -2,16 +2,19 @@ package com.jalbarracin.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.jalbarracin.Main;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.jalbarracin.FlappeeBeeGame;
 
 public class DesktopLauncher {
 
-	private static final float SIZE_FACTOR = 0.5f;
 
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = (int) (750 * SIZE_FACTOR);
-		config.height = (int) (1334 * SIZE_FACTOR);
-		new LwjglApplication(new Main(), config);
+		config.height = 320;
+		config.width = 240;
+
+		TexturePacker.process("../assets", "../assets", "flappee_bee_assets");
+
+		new LwjglApplication(new FlappeeBeeGame(), config);
 	}
 }
